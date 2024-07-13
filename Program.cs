28a01efadbb08b4722b02964215579330d003f2a
light2k4 <augustin.pro.prs@gmail.com>
2024-07-13 16:15:36 +0200

    v1.0.0 - First commit

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+internal class CleaNight
+{
+    internal static async Task Main(string[] _)
+    {
+        string repo = "Light2k4/CleaNight";
+        string latestReleaseInfo = await GetLatestReleaseInfo(repo);
+        Console.WriteLine(latestReleaseInfo);
+
+        string ascii = @"
+
+             ██████╗██╗     ███████╗ █████╗ ███╗   ██╗██╗ ██████╗ ██╗  ██╗████████╗
+            ██╔════╝██║     ██╔════╝██╔══██╗████╗  ██║██║██╔════╝ ██║  ██║╚══██╔══╝
+            ██║     ██║     █████╗  ███████║██╔██╗ ██║██║██║  ███╗███████║   ██║   
+            ██║     ██║     ██╔══╝  ██╔══██║██║╚██╗██║██║██║   ██║██╔══██║   ██║   
+            ╚██████╗███████╗███████╗██║  ██║██║ ╚████║██║╚██████╔╝██║  ██║   ██║   
+             ╚═════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   
+                                                                       
+";
+
+        string getVersionLocal = "1.0.0";
+        string getVersionGithub = latestReleaseInfo.Split(' ')[4];
+
+        string header = ascii + "\n" + "Version: " + getVersionLocal + "      Last version: " + getVersionGithub + "\n\n";
+
+        Console.Title = "CleaNight - github.com/Light2k4/CleaNight";
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(header);
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        bool continuer = true;
+        bool afficherMenu = true;
+        while (continuer)
+        {
+            if (afficherMenu)
+            {
+                Console.WriteLine("1. Supprimer les dossiers temporaires");
+                Console.WriteLine("2. Rejoindre le serveur Discord");
+                Console.WriteLine("3. Quitter");
+                Console.Write("\nVotre choix : ");
+            }
+
+            string choix = Console.ReadLine();
+
+            switch (choix)
+            {
+                case "1":
+                    SupprimerDossiers(header);
+                    afficherMenu = false;
+                    break;
+                case "2":
+                    System.Diagnostics.Process.Start("https://discord.gg/Acdp4B6KUv");
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(header);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    afficherMenu = false;
+                    break;
+                case "3":
+                    continuer = false;
+                    Console.WriteLine("Bye...");
+                    await Task.Delay(1000);
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Choix invalide.");
+                    await Task.Delay(1000);
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine(header);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    afficherMenu = true;
+                    break;
+            }
+        }
+    }
+
+    private static async Task<string> GetLatestReleaseInfo(string repo)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string url = $"https://api.github.com/repos/{repo}/releases/latest";
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("CleaNightApp/1.0 (HttpClient .NET)");
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var root = Newtonsoft.Json.Linq.JObject.Parse(responseBody);
+                string tagName = (string)root["tag_name"];
+                bool preRelease = (bool)root["prerelease"];
+
+                string versionType = preRelease ? "version Dev (pre release)" : "version Stable (release)";
+                return $"La dernière version est {tagName} et c'est une {versionType}.";
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Message :{0} ", e.Message);
+                return "Impossible de récupérer les informations de la dernière version.";
+            }
+        }
+    }
+
+    private static async void SupprimerDossiers(string header)
+    {
+        bool confirmation = DemanderConfirmation("Êtes-vous sûr de vouloir supprimer les dossiers temporaires ?");
+        if (!confirmation)
+        {
+            Console.WriteLine("Suppression annulée.");
+            return;
+        }
+
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine(header);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Suppression des dossiers en cours...");
+
+        string[] dossiers = Directory.GetDirectories(@"C:\Users\" + Environment.UserName + @"\AppData\Local\Temp");
+
+        foreach (string dossier in dossiers)
+        {
+            try
+            {
+                Directory.Delete(dossier, true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur lors de la suppression du dossier " + dossier + ": " + e.Message);
+            }
+        }
+
+        Console.WriteLine("\n\nSuppression terminée.");
+        Console.WriteLine("1. Laissez un avis");
+        Console.WriteLine("2. Quitter");
+        Console.Write("\nVotre choix : ");
+        string choixEnd = Console.ReadLine();
+
+        switch (choixEnd)
+        {
+            case "1":
+                System.Diagnostics.Process.Start("https://github.com/light2k4/CleaNight/");
+                await Task.Delay(1000);
+                Environment.Exit(0);
+                break;
+            case "2":
+                Console.WriteLine("Bye...");
+                await Task.Delay(1000);
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Choix invalide. \nBye...");
+                await Task.Delay(1000);
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    private static bool DemanderConfirmation(string message)
+    {
+        Console.Write($"{message} (O/N): ");
+        string reponse = Console.ReadLine();
+        return reponse.Equals("O", StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("CleaNight")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CleaNight")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("2c42e18e-85d3-45ee-a238-b99c72ccc02f")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
